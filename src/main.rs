a290fd92bdf02fc3eda700e9f62d959c8298d0c7
Paul Crowley <github@paul.ciphergoth.org>
2020-12-13 08:26:19 -0800

    Use gpio example code

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+// Copyright (c) 2017-2019 Rene van der Meer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// gpio_blinkled_signals.rs - Blinks an LED connected to a GPIO pin in a loop,
+// while handling any incoming SIGINT (Ctrl-C) and SIGTERM signals so the
+// pin's state can be reset before the application exits.
+//
+// Remember to add a resistor of an appropriate value in series, to prevent
+// exceeding the maximum current rating of the GPIO pin and the LED.
+
+use std::error::Error;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+// The simple-signal crate is used to handle incoming signals.
+use simple_signal::{self, Signal};
+
+use rppal::gpio::Gpio;
+
+// Gpio uses BCM pin numbering.
+const GPIO_PUL: u8 = 13;
+const GPIO_DIR: u8 = 16;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Retrieve the GPIO pin and configure it as an output.
+    let mut pin = Gpio::new()?.get(GPIO_PUL)?.into_output();
+
+    let running = Arc::new(AtomicBool::new(true));
+
+    // When a SIGINT (Ctrl-C) or SIGTERM signal is caught, atomically set running to false.
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], {
+        let running = running.clone();
+        move |_| {
+            running.store(false, Ordering::SeqCst);
+        }
+    });
+
+    // Blink pulse
+    while running.load(Ordering::SeqCst) {
+        pin.set_high();
+        thread::sleep(Duration::from_millis(1));
+        pin.set_low();
+        thread::sleep(Duration::from_millis(500));
+    }
+
+    // After we're done blinking, turn the LED off.
+    pin.set_low();
+
+    Ok(())
+
+    // When the pin variable goes out of scope, the GPIO pin mode is automatically reset
+    // to its original value, provided reset_on_drop is set to true (default).
+}
