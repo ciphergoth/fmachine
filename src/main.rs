ab213d361a4fae0f46daec4868b8823e4f534148
Paul Crowley <github@paul.ciphergoth.org>
2020-12-18 22:10:16 -0800

    Move joystick out of thread

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::fs::File;
-use std::sync::atomic::{AtomicBool, AtomicI64, AtomicI8, AtomicU64, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 use anyhow::Result;
-use evdev_rs::Device;
 use rppal::gpio::Gpio;
-//use simple_signal::{self, Signal};
+use simple_signal::{self, Signal};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -23,7 +22,6 @@ struct Opt {
 struct Control {
     target_velocity: AtomicU64,
     accel: AtomicU64,
-
 }
 
 // Gpio uses BCM pin numbering.
@@ -35,6 +33,7 @@ const PULSE_DURATION: Duration = Duration::from_micros(PULSE_DURATION_US);
 const MIN_T: f64 = 0.0001;
 const STEPS: u64 = 16000;
 const CONTROL_FACTOR: f64 = 0.001;
+const CONTROL_SLEEP: Duration = Duration::from_micros(100000);
 
 fn device(ctrl: Arc<Control>) -> Result<()> {
     let gpio = Gpio::new()?;
@@ -48,7 +47,9 @@ fn device(ctrl: Arc<Control>) -> Result<()> {
         let target_velocity = ctrl.target_velocity.load(Ordering::Relaxed) as f64 * CONTROL_FACTOR;
         let accel = ctrl.accel.load(Ordering::Relaxed) as f64 * CONTROL_FACTOR;
         let max_delta_v = accel * t;
-        let delta_v = (target_velocity - velocity_hz).min(max_delta_v).max(-max_delta_v);
+        let delta_v = (target_velocity - velocity_hz)
+            .min(max_delta_v)
+            .max(-max_delta_v);
         let new_vel = velocity_hz + delta_v;
         let delta_v = if new_vel * new_vel / accel < (i * 2) as f64 {
             delta_v
@@ -61,7 +62,8 @@ fn device(ctrl: Arc<Control>) -> Result<()> {
             break;
         }
         t = (1.0 + delta_v * t / 2.0) / velocity_hz;
-        if t < MIN_T { // this should never happen
+        if t < MIN_T {
+            // this should never happen
             t = MIN_T;
         }
         pul_pin.set_high();
@@ -76,53 +78,58 @@ fn device(ctrl: Arc<Control>) -> Result<()> {
     Ok(())
 }
 
-fn joystick(opt: Opt, ctrl: Arc<Control>, run: Arc<AtomicBool>) -> Result<()> {
-    let f = File::open("/dev/input/event0").unwrap();
-    
-    let mut d = Device::new().unwrap();
-    d.set_fd(f).unwrap();
+fn joystick(opt: Opt) -> Result<()> {
     let mut target_velocity = opt.max_velocity;
+    let ctrl = Arc::new(Control {
+        target_velocity: AtomicU64::new((opt.max_velocity / CONTROL_FACTOR) as u64),
+        accel: AtomicU64::new((opt.max_accel / CONTROL_FACTOR) as u64),
+    });
+    let run = Arc::new(AtomicBool::new(true));
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], {
+        let run = run.clone();
+        move |_| {
+            run.store(false, Ordering::Relaxed);
+        }
+    });
+    let device_thread = {
+        let ctrl = ctrl.clone();
+        thread::spawn(move || device(ctrl))
+    };
+
+    let fd = File::open("/dev/input/event0")?;
+    let ev_device = evdev_rs::Device::new_from_fd(fd)?;
     while run.load(Ordering::Relaxed) {
-        let a = d.next_event(evdev_rs::ReadFlag::NORMAL | evdev_rs::ReadFlag::BLOCKING);
+        let a = ev_device.next_event(evdev_rs::ReadFlag::NORMAL);
         match a {
             Ok(k) => {
-                //println!("Event: {:?}", k.1);
-                match k.1.event_code {
-                    evdev_rs::enums::EventCode::EV_ABS(evdev_rs::enums::EV_ABS::ABS_RY) => {
-                        target_velocity = (target_velocity + (k.1.value as f64)).max(0.0).min(opt.max_velocity);
-                        println!("val: {} target_velocity: {}", k.1.value, target_velocity);
-                        ctrl.target_velocity.store((target_velocity / CONTROL_FACTOR) as u64, Ordering::Relaxed);
-                    }
-                    _ => (),
+                println!("Event: {:?}", k.1);
+                if let evdev_rs::enums::EventCode::EV_ABS(evdev_rs::enums::EV_ABS::ABS_RY) =
+                    k.1.event_code
+                {
+                    target_velocity = (target_velocity - 0.01 * (k.1.value as f64))
+                        .max(0.0)
+                        .min(opt.max_velocity);
+                    println!("val: {} target_velocity: {}", k.1.value, target_velocity);
+                    ctrl.target_velocity
+                        .store((target_velocity / CONTROL_FACTOR) as u64, Ordering::Relaxed);
                 }
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                thread::sleep(CONTROL_SLEEP);
             },
-            Err(e) => println!("{:?}", e),
         }
     }
+    println!("Run is false, stopping");
+    ctrl.target_velocity.store(0, Ordering::Relaxed);
+    device_thread.join().unwrap()?;
     Ok(())
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
     println!("{:?}", opt);
-    let ctrl = Arc::new(Control {
-        target_velocity: AtomicU64::new((opt.max_velocity / CONTROL_FACTOR) as u64),
-        accel: AtomicU64::new((opt.max_accel / CONTROL_FACTOR) as u64)
-    });
-    let run = Arc::new(AtomicBool::new(true));
-    let device_thread = {
-        let ctrl = ctrl.clone();
-        thread::spawn(move || device(ctrl))
-    };
-
-    let joystick_thread = {
-        let ctrl = ctrl.clone();
-        let run = run.clone();
-        thread::spawn(move || joystick(opt, ctrl, run))
-    };
-    device_thread.join().unwrap()?;
-    run.store(false, Ordering::Relaxed);
-    joystick_thread.join().unwrap()?;
+    joystick(opt)?;
     println!("Finished successfully");
     Ok(())
 }
