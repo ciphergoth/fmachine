6875946332a6bc0f53627838fe25e8532d014be0
Paul Crowley <github@paul.ciphergoth.org>
2020-12-18 21:13:36 -0800

    Actual joystick control

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ const GPIO_PUL: u8 = 13;
 const PULSE_DURATION_US: u64 = 1;
 const PULSE_DURATION: Duration = Duration::from_micros(PULSE_DURATION_US);
 const MIN_T: f64 = 0.0001;
-const STEPS: u64 = 1600;
+const STEPS: u64 = 16000;
 const CONTROL_FACTOR: f64 = 0.001;
 
 fn device(ctrl: Arc<Control>) -> Result<()> {
@@ -76,15 +76,26 @@ fn device(ctrl: Arc<Control>) -> Result<()> {
     Ok(())
 }
 
-fn joystick(run: Arc<AtomicBool>) -> Result<()> {
+fn joystick(opt: Opt, ctrl: Arc<Control>, run: Arc<AtomicBool>) -> Result<()> {
     let f = File::open("/dev/input/event0").unwrap();
-
+    
     let mut d = Device::new().unwrap();
     d.set_fd(f).unwrap();
+    let mut target_velocity = opt.max_velocity;
     while run.load(Ordering::Relaxed) {
         let a = d.next_event(evdev_rs::ReadFlag::NORMAL | evdev_rs::ReadFlag::BLOCKING);
         match a {
-            Ok(k) => println!("Event: {:?}", k.1),
+            Ok(k) => {
+                //println!("Event: {:?}", k.1);
+                match k.1.event_code {
+                    evdev_rs::enums::EventCode::EV_ABS(evdev_rs::enums::EV_ABS::ABS_RY) => {
+                        target_velocity = (target_velocity + (k.1.value as f64)).max(0.0).min(opt.max_velocity);
+                        println!("val: {} target_velocity: {}", k.1.value, target_velocity);
+                        ctrl.target_velocity.store((target_velocity / CONTROL_FACTOR) as u64, Ordering::Relaxed);
+                    }
+                    _ => (),
+                }
+            },
             Err(e) => println!("{:?}", e),
         }
     }
@@ -105,8 +116,9 @@ fn main() -> Result<()> {
     };
 
     let joystick_thread = {
+        let ctrl = ctrl.clone();
         let run = run.clone();
-        thread::spawn(move || joystick(run))
+        thread::spawn(move || joystick(opt, ctrl, run))
     };
     device_thread.join().unwrap()?;
     run.store(false, Ordering::Relaxed);
