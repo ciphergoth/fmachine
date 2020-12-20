25e261f6b9318f9e7845792bd1eca846fcde726f
Paul Crowley <github@paul.ciphergoth.org>
2020-12-20 09:21:40 -0800

    tick every second with select

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::fs::OpenOptions;
 use std::io::ErrorKind;
 use std::os::unix::fs::OpenOptionsExt;
+use std::time::Duration;
 
 use anyhow::{anyhow, Result};
-use tokio::io::unix::AsyncFd;
 use tokio::io::Interest;
+use tokio::{io::unix::AsyncFd, time};
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
+    let mut interval = time::interval(Duration::from_secs(1));
     let fd = OpenOptions::new()
         .read(true)
         .custom_flags(libc::O_NONBLOCK)
@@ -18,21 +20,28 @@ pub async fn main() -> Result<()> {
         Interest::READABLE,
     )?;
     loop {
-        let mut guard = afd.readable().await?;
+        tokio::select! {
+            r = afd.readable() => {
+                let mut guard = r?;
 
-        let a = ev_device.next_event(evdev_rs::ReadFlag::NORMAL);
-        match a {
-            Ok(k) => {
-                println!("Event: {:?}", k.1);
-                guard.retain_ready();
+                let a = ev_device.next_event(evdev_rs::ReadFlag::NORMAL);
+                match a {
+                    Ok(k) => {
+                        println!("Event: {:?}", k.1);
+                        guard.retain_ready();
+                    }
+                    Err(e) if e.kind() == ErrorKind::WouldBlock => {
+                        println!("would block");
+                        guard.clear_ready();
+                    }
+                    not_ok => {
+                        println!("boom");
+                        not_ok?;
+                    }
+                }
             }
-            Err(e) if e.kind() == ErrorKind::WouldBlock => {
-                println!("would block");
-                guard.clear_ready();
-            }
-            not_ok => {
-                println!("boom");
-                not_ok?;
+            _ = interval.tick() => {
+                println!("tick");
             }
         }
     }
