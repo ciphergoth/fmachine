c406dc46279c4327dea3c1e84c19273cf4eb86b6
Paul Crowley <github@paul.ciphergoth.org>
2020-12-22 17:44:37 -0800

    Axis handing struct

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,48 @@ use tokio::{io::unix::AsyncFd, time};
 
 mod timeval;
 
+#[derive(Debug)]
+struct Axis {
+    event_code: evdev_rs::enums::EventCode,
+    abs_info: evdev_rs::AbsInfo,
+    driven: f64,
+    last_read: Option<(evdev_rs::TimeVal, i32)>,
+}
+
+impl Axis {
+    fn new(ev_device: &evdev_rs::Device, event_code: evdev_rs::enums::EventCode) -> Result<Axis> {
+        Ok(Axis {
+            event_code,
+            abs_info: ev_device.abs_info(&event_code).ok_or_else(|| anyhow!("wtf"))?,
+            driven: 0.0,
+            last_read: None
+        })
+    }
+
+    fn handle_event(&mut self, event: &evdev_rs::InputEvent) {
+        if event.event_code == self.event_code {
+            if let Some((t, v)) = self.last_read {
+                self.driven += v as f64 * timeval::diff_as_f64(&event.time, &t);
+            }
+            let new_v = if event.value <= self.abs_info.flat && event.value >= -self.abs_info.flat {
+                0
+            } else {
+                event.value
+            };
+            self.last_read = Some((event.time, new_v));
+            println!("driven: {} stick {} new_v {}", self.driven, event.value, new_v);
+        }
+    }
+
+    fn handle_tick(&mut self, now: evdev_rs::TimeVal) {
+        if let Some((t, v)) = self.last_read {
+            self.driven += v as f64 * timeval::diff_as_f64(&now, &t);
+            println!("driven: {}", self.driven);
+            self.last_read = Some((now, v));
+        }
+    }
+}
+
 #[tokio::main]
 pub async fn main() -> Result<()> {
     let mut interval = time::interval(Duration::from_millis(50));
@@ -17,18 +59,16 @@ pub async fn main() -> Result<()> {
         .custom_flags(libc::O_NONBLOCK)
         .open("/dev/input/event0")?;
     let ev_device = evdev_rs::Device::new_from_file(fd)?;
-    let abs_x = evdev_rs::enums::EventCode::EV_ABS(evdev_rs::enums::EV_ABS::ABS_X);
-    let ai = ev_device.abs_info(&abs_x).ok_or_else(|| anyhow!("wtf"))?;
+    let mut xaxis = Axis::new(&ev_device,
+        evdev_rs::enums::EventCode::EV_ABS(evdev_rs::enums::EV_ABS::ABS_X))?;
     println!(
         " min {} max {} fuzz {} flat {} res {}",
-        ai.minimum, ai.maximum, ai.fuzz, ai.flat, ai.resolution
+        xaxis.abs_info.minimum, xaxis.abs_info.maximum, xaxis.abs_info.fuzz, xaxis.abs_info.flat, xaxis.abs_info.resolution
     );
     let afd = AsyncFd::with_interest(
         ev_device,
         Interest::READABLE,
     )?;
-    let mut driven = 0.0;
-    let mut last_read = None;
     loop {
         tokio::select! {
             r = afd.readable() => {
@@ -38,19 +78,8 @@ pub async fn main() -> Result<()> {
                 match a {
                     Ok(k) => {
                         guard.retain_ready();
+                        xaxis.handle_event(&k.1);
                         //println!("Event: {:?}", k.1);
-                        if k.1.event_code == abs_x {
-                            if let Some((t, v)) = last_read {
-                                driven += v as f64 * timeval::diff_as_f64(&k.1.time, &t);
-                            }
-                            let new_v = if k.1.value <= ai.flat && k.1.value >= -ai.flat {
-                                0
-                            } else {
-                                k.1.value
-                            };
-                            last_read = Some((k.1.time, new_v));
-                            println!("driven: {} stick {} new_v {}", driven, k.1.value, new_v);
-                        }
                     }
                     Err(e) if e.kind() == ErrorKind::WouldBlock => {
                         //println!("would block");
@@ -65,11 +94,7 @@ pub async fn main() -> Result<()> {
             _ = interval.tick() => {
                 let now = timeval::now()?;
                 //println!("tick {:?}", now);
-                if let Some((t, v)) = last_read {
-                    driven += v as f64 * timeval::diff_as_f64(&now, &t);
-                    println!("driven: {}", driven);
-                    last_read = Some((now, v));
-                }
+                xaxis.handle_tick(now);
             }
         }
     }
